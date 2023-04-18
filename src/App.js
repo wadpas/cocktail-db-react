4085abbfb86b0eaed3f8eb0728678025ab44da31
wadpas <wadpas@gmail.com>
2023-04-18 22:43:40 +0300

    added UserList comp.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Counter } from './components/Counter'
+import { UserList } from './components/UserList'
 
 function App() {
   return (
     <div className="container">
       <Counter />
+      <UserList />
     </div>
   )
 }
