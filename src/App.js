87d454547c2ca1e8e966dd6d5c85ef12eaba4125
wadpas <wadpas@gmail.com>
2023-04-19 12:19:20 +0300

    added Form comp.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Counter } from './components/Counter'
 import { UserList } from './components/UserList'
+import { Form } from './components/Form'
 
 function App() {
   return (
     <div className="container">
       <Counter />
+      <Form />
       <UserList />
     </div>
   )
---
+++ src/components/Form.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+export const Form = () => {
+  const [firstName, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+  const [people, setPeople] = useState([])
+
+  const handleSubmite = (e) => {
+    e.preventDefault()
+    if (firstName && email) {
+      const person = { firstName, email }
+      setPeople((people) => {
+        return [...people, person]
+      })
+    }
+  }
+
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmite}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name : </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value)
+              }}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }}
+            />
+          </div>
+          <button type="submit">Add person</button>
+        </form>
+      </article>
+    </>
+  )
+}
