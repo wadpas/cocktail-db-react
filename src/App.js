298e340f60b3348a106621f75ce1f0528d78d900
wadpas <wadpas@gmail.com>
2023-04-24 10:31:28 +0300

    added caffe & cv comp.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Birthdays from './components/birthdays/Birthdays'
+import CV from './components/cv/CV'
 import Tours from './components/tours/Tours'
 import Review from './components/review/Review'
 import Accordion from './components/accordion/Accordion'
+import Caffe from './components/caffe/Caffe'
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="random-btn" to="/birthdays">
-          Birthdays
+      <div className="title">
+        <Link className="random-btn" to="/cv">
+          CV
         </Link>
         <Link className="random-btn" to="/tours">
           Tours
@@ -17,15 +18,19 @@ function App() {
         <Link className="random-btn" to="/review">
           Review
         </Link>
-        <Link className="random-btn" to="/">
+        <Link className="random-btn" to="/accordion">
           Accordion
         </Link>
-      </header>
+        <Link className="random-btn" to="/">
+          Caffe
+        </Link>
+      </div>
       <Routes>
-        <Route path="/birthdays" element={<Birthdays />} />
+        <Route path="/cv" element={<CV />} />
         <Route path="/tours" element={<Tours />} />
         <Route path="/review" element={<Review />} />
-        <Route path="/" element={<Accordion />} />
+        <Route path="/accordion" element={<Accordion />} />
+        <Route path="/" element={<Caffe />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/caffe/Caffe.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
+import './index.css'
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+
+function Caffe() {
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
+
+  return (
+    <section className="menu section">
+      <div className="title">
+        <h2>our menu</h2>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </div>
+    </section>
+  )
+}
+
+export default Caffe
