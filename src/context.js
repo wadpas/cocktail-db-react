373f8f7c0663ec278ff3fb9dba2d31d7f6534238
wadpas <wadpas@gmail.com>
2023-04-25 13:35:55 +0300

    implemented fetch & list

--- src/context.js
+++ src/context.js
@@ -2,12 +2,52 @@ import React, { useState, useContext, useEffect } from 'react'
 import { useCallback } from 'react'
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [loading, setLoading] = useState(true)
+  const [search, setSearch] = useState('a')
+  const [cocktails, setCocktails] = useState([])
+
+  const fetchDrinks = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(`${url}${search}`)
+      const data = await response.json()
+      const { drinks } = data
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          }
+        })
+        setCocktails(newCocktails)
+      } else {
+        setCocktails([])
+      }
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+    }
+  }
+
+  useState(() => {
+    fetchDrinks()
+  }, [search])
+
+  return (
+    <AppContext.Provider value={{ loading, search, cocktails, setCocktails }}>
+      {children}
+    </AppContext.Provider>
+  )
 }
-// make sure use
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
