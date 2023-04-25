8908cb380322964eb9c6451c34798235662783d9
wadpas <wadpas@gmail.com>
2023-04-25 10:53:54 +0300

    added about & error content

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">abot us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis labore
+        asperiores tempora quibusdam perspiciatis facere, consectetur quisquam
+        vel quis aliquam blanditiis iusto nesciunt accusantium alias, amet
+        beatae ab fugiat placeat doloremque! Culpa, veritatis expedita! Magnam
+        obcaecati alias facere minima nulla?
+      </p>
+    </section>
   )
 }
 
